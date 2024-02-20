08f88a83dedec30e9b17ea7fa4a2dc4f1a05c410
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-02-20 11:31:10 +0600

    Navber part done

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -1,10 +1,35 @@
-
+import img from '../../../public/Logo.png'
 const Navber = () => {
-    return (
-        <div>
-            <h1></h1>
+  return (
+    <div>
+      <div className="my-6 p-4">
+        <div className="navbar bg-base-1 gap-3">
+          <div className="navbar-start">
+            <a href="#">
+              <img src={img} alt="" />
+            </a>
+          </div>
+          <div className="navbar-center">
+            <a
+              id="sortButton"
+              data-sortByview="all"
+              onClick="sortbyview()"
+              className="btn"
+            >
+              Sort by view
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <a href="./blog.html" className="btn bg-[#FF1F3D]">
+              Blog
+            </a>
+          </div>
         </div>
-    );
+      </div>
+      <hr />
+    </div>
+  );
 };
 
 export default Navber;
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Main from "../Components/Main/Main";
 
 
 const Root = () => {
     return (
         <div>
-            
+            <Main></Main>
+            <Outlet></Outlet>
         </div>
     );
 };
